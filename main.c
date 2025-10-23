9dbdca8c543a6494a2fdc89e7d479cde322f8994
Mohamed100100 <muhammad.ahmed302@eng-st.cu.edu.eg>
2025-10-24 00:53:55 +0300

    this commit from modelus branch and it has the modelus feature

--- main.c
+++ main.c
@@ -4,11 +4,12 @@
 
 int multiply(int a,int b);
 int add(int a,int b);
+int mod(int a,int b);
 
 int main(){
    printf("%i",multiply(2,5));
    printf("%i" , add(2*5));
-
+   printf("%i",mod(2,5));
 }
 
 int multiply(int a,int b){
@@ -17,4 +18,8 @@ int multiply(int a,int b){
 
 int add(int a,int b){
    return (a+b);
+}
+
+int mod(int a,int b){
+   return(a%b);
 }
