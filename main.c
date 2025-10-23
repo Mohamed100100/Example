d92774efb24209b577280d99d9dc2dce03ba32b4
Mohamed100100 <muhammad.ahmed302@eng-st.cu.edu.eg>
2025-10-24 00:43:41 +0300

    this commit from Division branch and it has the Divide feature

--- main.c
+++ main.c
@@ -2,13 +2,14 @@
 
 #include "stdio.h"
 
+int divide(int a,int b)
 int multiply(int a,int b);
 int add(int a,int b);
 
 int main(){
    printf("%i",multiply(2,5));
    printf("%i" , add(2*5));
-
+   printf("%i",divide(2/5));
 }
 
 int multiply(int a,int b){
@@ -17,4 +18,9 @@ int multiply(int a,int b){
 
 int add(int a,int b){
    return (a+b);
+}
+
+
+int divide(int a,int b){
+   return (a/b);
 }
